32535f6b99fccbf4e73f0019257887d73b8e6024
MSalvaris <msalvaris@users.noreply.github.com>
2018-09-16 17:47:48 +0100

    Adds models module

---
+++ src/model.py
@@ -0,0 +1,16 @@
+from git import Repo
+import os
+
+
+def _ver():
+    repo = Repo()
+    # repo.active_branch.commit.hexsha
+    return repo.active_branch.name
+
+
+def model_path():
+    model_path = os.path.join(os.getenv('MODEL'), _ver())
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+    return model_path
+    
