3006944a983a5c28e92eedfb49685939694e0dbe
MSalvaris <msalvaris@users.noreply.github.com>
2018-09-16 19:51:23 +0100

    Adds another iou metric

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import tensorflow as tf
 
 def cov_to_class(val):
     for i in range(0, 11):
@@ -79,3 +79,24 @@ def iou_metric_batch(y_true_in, y_pred_in):
         value = iou_metric(y_true_in[batch], y_pred_in[batch])
         metric.append(value)
     return np.mean(metric)
+
+
+def get_iou_vector(A, B):
+    batch_size = A.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        t, p = A[batch] > 0, B[batch] > 0
+        intersection = np.logical_and(t, p)
+        union = np.logical_or(t, p)
+        iou = (np.sum(intersection > 0) + 1e-10) / (np.sum(union > 0) + 1e-10)
+        thresholds = np.arange(0.5, 1, 0.05)
+        s = []
+        for thresh in thresholds:
+            s.append(iou > thresh)
+        metric.append(np.mean(s))
+
+    return np.mean(metric)
+
+
+def my_iou_metric(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred>0.5], tf.float64)
