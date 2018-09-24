a3636a9db4a04bb73c1be0cb8bb24f593ceeda16
MSalvaris <msalvaris@users.noreply.github.com>
2018-09-24 09:10:11 +0100

    Adds log path method to utils

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,13 @@
-import torch
+import os
+
 import numpy as np
+import torch
+from git import Repo
 
 
 def cosine_annealing(step, total_steps, lr_max, lr_min):
     return lr_min + (lr_max - lr_min) * 0.5 * (
-        1 + np.cos(step / total_steps * np.pi))
+            1 + np.cos(step / total_steps * np.pi))
 
 
 def _get_optimizer(model_parameters, optim_config):
@@ -33,7 +36,7 @@ def _get_scheduler(optimizer, optim_config):
                 gamma=optim_config['lr_decay'])
         elif optim_config['scheduler'] == 'cosine':
             total_steps = optim_config['epochs'] * \
-                optim_config['steps_per_epoch']
+                          optim_config['steps_per_epoch']
 
             scheduler = torch.optim.lr_scheduler.LambdaLR(
                 optimizer,
@@ -51,3 +54,16 @@ def create_optimizer(model_parameters, optim_config):
     optimizer = _get_optimizer(model_parameters, optim_config)
     scheduler = _get_scheduler(optimizer, optim_config)
     return optimizer, scheduler
+
+
+def _ver():
+    repo = Repo(search_parent_directories=True)
+    # repo.active_branch.commit.hexsha
+    return repo.active_branch.name
+
+
+def tboard_log_path():
+    log_path = os.path.join(os.getenv('TBOARD_LOGS'), _ver())
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    return log_path
