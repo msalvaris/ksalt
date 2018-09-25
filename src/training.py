06ccc914744fa6964b93597fd02858e334cb0f63
MSalvaris <msalvaris@users.noreply.github.com>
2018-09-25 14:41:48 +0100

    Adds mask and predictions to the tensorboard output for train and test

--- src/training.py
+++ src/training.py
@@ -8,13 +8,21 @@ import torchvision
 
 logger = logging.getLogger(__name__)
 
+def create_image_writer(summary_writer):
+    def write_to(data_tensor, label, epoch, normalize=False):
+        image_grid = torchvision.utils.make_grid(
+            data_tensor, normalize=normalize, scale_each=True)
+        summary_writer.add_image(label, image_grid, epoch)
+    return write_to
+    
 
 def train(epoch, model, optimizer, scheduler, criterion, train_loader, config, summary_writer=None):
     logger.info('Train {}'.format(epoch))
 
     run_config = config['run_config']
     optim_config = config['optim_config']
-    
+
+    image_writer = create_image_writer(summary_writer)
     model.train()
 
     train_metrics = defaultdict(list)
@@ -22,9 +30,7 @@ def train(epoch, model, optimizer, scheduler, criterion, train_loader, config, s
     for step, (image, mask) in enumerate(train_loader):
         train.global_step+=1
         if summary_writer is not None and step == 0:
-            image_grid = torchvision.utils.make_grid(
-                image, normalize=True, scale_each=True)
-            summary_writer.add_image('Train/Image', image_grid, epoch)
+            image_writer(image, 'Train/Image', epoch, normalize=True)
 
         if optim_config['scheduler'] == 'multistep':
             scheduler.step(epoch - 1)
@@ -45,9 +51,14 @@ def train(epoch, model, optimizer, scheduler, criterion, train_loader, config, s
 
         loss.backward()
         optimizer.step()
-        
+
+        output_cpu = output.cpu()
         train_metrics['loss'].append(loss.item())
-        train_metrics['iou'].append(my_iou_metric(mask.numpy(), output.cpu().data.numpy()))
+        train_metrics['iou'].append(my_iou_metric(mask.numpy(), output_cpu.numpy()))
+        
+        if summary_writer is not None and step == 0:
+            image_writer(mask, 'Train/Mask', epoch)
+            image_writer(output_cpu, 'Train/Prediction', epoch)
 
         if summary_writer:
             summary_writer.add_scalar('Train/RunningLoss', train_metrics['loss'][-1], train.global_step)
@@ -81,27 +92,33 @@ def train(epoch, model, optimizer, scheduler, criterion, train_loader, config, s
 def test(epoch, model, criterion, test_loader, summary_writer=None):
     logger.info('Test {}'.format(epoch))
 
+    image_writer = create_image_writer(summary_writer)
+    
     model.eval()
 
     val_metrics = defaultdict(list)
     start = time.time()
     for step, (image, mask) in enumerate(test_loader):
 
-        if summary_writer and epoch == 0 and step == 0:
-            image_grid = torchvision.utils.make_grid(
-                image, normalize=True, scale_each=True)
-            summary_writer.add_image('Test/Image', image_grid, epoch)
-        
+        if summary_writer is not None and step == 0:
+            image_writer(image, 'Test/Image', epoch, normalize=True)
+            
         with torch.cuda.device(0):
             image = image.type(torch.float).cuda(async=True)
             mask_gpu = mask.type(torch.float).cuda(async=True)
         
         with torch.no_grad():
-            outputs = model(image)
-        loss = criterion(outputs, mask_gpu)
+            output = model(image)
+        loss = criterion(output, mask_gpu)
+
+        output_cpu = output.cpu()
+
+        if summary_writer is not None and step == 0:
+            image_writer(mask, 'Test/Mask', epoch)
+            image_writer(output_cpu, 'Test/Prediction', epoch)
 
         val_metrics['loss'].append(loss.item())
-        val_metrics['iou'].append(my_iou_metric(mask.numpy(), outputs.cpu().data.numpy()))
+        val_metrics['iou'].append(my_iou_metric(mask.numpy(), output_cpu.data.numpy()))
 
     message = (
         f"Epoch: {epoch},"
