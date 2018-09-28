bf7b83a4d3733bb7245bb381bbbb49a98c9073a6
MSalvaris <msalvaris@users.noreply.github.com>
2018-09-28 10:16:08 +0100

    Refactored for readability

--- src/training.py
+++ src/training.py
@@ -10,10 +10,12 @@ from metrics import my_iou_metric
 
 logger = logging.getLogger(__name__)
 
+
 def create_image_writer(summary_writer):
     def write_to(data_tensor, label, epoch, normalize=False):
         image_grid = torchvision.utils.make_grid(
-            data_tensor, normalize=normalize, scale_each=True)
+            data_tensor, normalize=normalize, scale_each=True
+        )
         summary_writer.add_image(label, image_grid, epoch)
 
     return write_to
@@ -25,17 +27,18 @@ def _add_metrics(metric_collector_dict, metrics_funcs, mask_array, pred_array):
     return metric_collector_dict
 
 
-def train(epoch,
-          model,
-          optimizer,
-          scheduler,
-          criterion,
-          train_loader,
-          config,
-          summary_writer=None,
-          global_counter=None,
-          metrics_funcs=(('iou', my_iou_metric),)):
-    logger.info('Train {}'.format(epoch))
+def train(
+    epoch,
+    model,
+    optimizer,
+    scheduler,
+    criterion,
+    train_loader,
+    summary_writer=None,
+    global_counter=None,
+    metrics_funcs=(("iou", my_iou_metric),),
+):
+    logger.info("Train {}".format(epoch))
 
     image_writer = create_image_writer(summary_writer)
     model.train()
@@ -45,13 +48,13 @@ def train(epoch,
     for step, (image, mask) in enumerate(train_loader):
         global_step = next(global_counter) if global_counter is not None else step
         scheduler.step()
-        
-        if summary_writer is not None:
-            summary_writer.add_scalar('Train/LearningRate',
-                                      scheduler.get_lr()[0], global_step)
-            if step == 0:
-                image_writer(image, 'Train/Image', epoch, normalize=True)
 
+        if summary_writer is not None:
+            summary_writer.add_scalar(
+                "Train/LearningRate", scheduler.get_lr()[0], global_step
+            )
+            if step == 0:
+                image_writer(image, "Train/Image", epoch, normalize=True)
 
         with torch.cuda.device(0):
             image = image.type(torch.float).cuda(async=True)
@@ -65,15 +68,21 @@ def train(epoch,
         optimizer.step()
 
         output_cpu = output.cpu()
-        train_metrics['loss'].append(loss.item())
-        train_metrics = _add_metrics(train_metrics, metrics_funcs, mask.numpy(), output_cpu.data.numpy())
+        train_metrics["loss"].append(loss.item())
+        train_metrics = _add_metrics(
+            train_metrics, metrics_funcs, mask.numpy(), output_cpu.data.numpy()
+        )
 
         if summary_writer is not None:
-            summary_writer.add_scalar('Train/RunningLoss', train_metrics['loss'][-1], global_step)
-            summary_writer.add_scalar('Train/RunningIoU', train_metrics['iou'][-1], global_step)
+            summary_writer.add_scalar(
+                "Train/RunningLoss", train_metrics["loss"][-1], global_step
+            )
+            summary_writer.add_scalar(
+                "Train/RunningIoU", train_metrics["iou"][-1], global_step
+            )
             if step == 0:
-                image_writer(mask, 'Train/Mask', epoch)
-                image_writer(output_cpu, 'Train/Prediction', epoch)
+                image_writer(mask, "Train/Mask", epoch)
+                image_writer(output_cpu, "Train/Prediction", epoch)
 
         if step % 100 == 0:
             message = (
@@ -90,23 +99,25 @@ def train(epoch,
         f"Train: loss {np.mean(train_metrics['loss']):.3f},  IoU {np.mean(train_metrics['iou']):.3f}"
     )
     logger.info(message)
-    logger.info('Elapsed {:.2f}'.format(elapsed))
+    logger.info("Elapsed {:.2f}".format(elapsed))
 
     if summary_writer:
-        summary_writer.add_scalar('Train/Loss', np.mean(train_metrics['loss']), epoch)
-        summary_writer.add_scalar('Train/IoU', np.mean(train_metrics['iou']), epoch)
-        summary_writer.add_scalar('Train/Time', elapsed, epoch)
+        summary_writer.add_scalar("Train/Loss", np.mean(train_metrics["loss"]), epoch)
+        summary_writer.add_scalar("Train/IoU", np.mean(train_metrics["iou"]), epoch)
+        summary_writer.add_scalar("Train/Time", elapsed, epoch)
 
     return train_metrics
 
 
-def test(epoch, 
-         model, 
-         criterion, 
-         test_loader, 
-         summary_writer=None, 
-         metrics_funcs=(('iou', my_iou_metric),)):
-    logger.info('Test {}'.format(epoch))
+def test(
+    epoch,
+    model,
+    criterion,
+    test_loader,
+    summary_writer=None,
+    metrics_funcs=(("iou", my_iou_metric),),
+):
+    logger.info("Test {}".format(epoch))
 
     image_writer = create_image_writer(summary_writer)
 
@@ -117,7 +128,7 @@ def test(epoch,
     for step, (image, mask) in enumerate(test_loader):
 
         if summary_writer is not None and step == 0:
-            image_writer(image, 'Test/Image', epoch, normalize=True)
+            image_writer(image, "Test/Image", epoch, normalize=True)
 
         with torch.cuda.device(0):
             image = image.type(torch.float).cuda(async=True)
@@ -129,11 +140,13 @@ def test(epoch,
 
         output_cpu = output.cpu()
         if summary_writer is not None and step == 0:
-            image_writer(mask, 'Test/Mask', epoch)
-            image_writer(output_cpu, 'Test/Prediction', epoch)
+            image_writer(mask, "Test/Mask", epoch)
+            image_writer(output_cpu, "Test/Prediction", epoch)
 
-        val_metrics['loss'].append(loss.item())
-        val_metrics = _add_metrics(val_metrics, metrics_funcs, mask.numpy(), output_cpu.data.numpy())
+        val_metrics["loss"].append(loss.item())
+        val_metrics = _add_metrics(
+            val_metrics, metrics_funcs, mask.numpy(), output_cpu.data.numpy()
+        )
 
     message = (
         f"Epoch: {epoch},"
@@ -142,11 +155,11 @@ def test(epoch,
     logger.info(message)
 
     elapsed = time.time() - start
-    logger.info('Elapsed {:.2f}'.format(elapsed))
+    logger.info("Elapsed {:.2f}".format(elapsed))
 
     if summary_writer:
-        summary_writer.add_scalar('Test/Loss', np.mean(val_metrics['loss']), epoch)
-        summary_writer.add_scalar('Test/IoU', np.mean(val_metrics['iou']), epoch)
-        summary_writer.add_scalar('Test/Time', elapsed, epoch)
+        summary_writer.add_scalar("Test/Loss", np.mean(val_metrics["loss"]), epoch)
+        summary_writer.add_scalar("Test/IoU", np.mean(val_metrics["iou"]), epoch)
+        summary_writer.add_scalar("Test/Time", elapsed, epoch)
 
     return val_metrics
