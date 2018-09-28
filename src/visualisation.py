d3644280bfa6ca3bf5268aba14ba78466307f3c6
MSalvaris <msalvaris@users.noreply.github.com>
2018-09-28 13:58:30 +0100

    Adds plot_poor_predictions method

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+
+from metrics import _thresholded_iou_for
 
 
 def plot_coverage_and_coverage_class(coverage, coverage_class):
@@ -9,24 +12,24 @@ def plot_coverage_and_coverage_class(coverage, coverage_class):
     plt.suptitle("Salt coverage")
     axs[0].set_xlabel("Coverage")
     axs[1].set_xlabel("Coverage class")
-    
+
 
 def scatter_coverage_and_coverage_class(coverage, coverage_class):
     plt.scatter(coverage, coverage_class)
     plt.xlabel("Coverage")
     plt.ylabel("Coverage class")
-    
-    
+
+
 def plot_depth_distributions(train_depth, test_depth):
     sns.distplot(train_depth, label="Train")
     sns.distplot(test_depth, label="Test")
     plt.legend()
     plt.title("Depth distribution")
-    
-    
-def plot_images(train_df, img_size_ori=101, max_images = 60, grid_width = 15, figsize=()):
+
+
+def plot_images(train_df, img_size_ori=101, max_images=60, grid_width=15, figsize=()):
     grid_height = int(max_images / grid_width)
-    figsize = (grid_width, grid_height) if figsize==() else figsize
+    figsize = (grid_width, grid_height) if figsize == () else figsize
     fig, axs = plt.subplots(grid_height, grid_width, figsize=figsize)
     for i, idx in enumerate(train_df.index[:max_images]):
         img = train_df.loc[idx].images
@@ -35,14 +38,33 @@ def plot_images(train_df, img_size_ori=101, max_images = 60, grid_width = 15, fi
         ax.imshow(img, cmap="Greys")
         ax.imshow(mask, alpha=0.3, cmap="Greens")
         ax.text(1, img_size_ori - 1, train_df.loc[idx].z, color="black")
-        ax.text(img_size_ori - 1, 1, round(train_df.loc[idx].coverage, 2), color="black", ha="right", va="top")
-        ax.text(1, 1, train_df.loc[idx].coverage_class, color="black", ha="left", va="top")
+        ax.text(
+            img_size_ori - 1,
+            1,
+            round(train_df.loc[idx].coverage, 2),
+            color="black",
+            ha="right",
+            va="top",
+        )
+        ax.text(
+            1, 1, train_df.loc[idx].coverage_class, color="black", ha="left", va="top"
+        )
         ax.set_yticklabels([])
         ax.set_xticklabels([])
-    plt.suptitle("Green: salt. Top-left: coverage class, top-right: salt coverage, bottom-left: depth")
-    
-    
-def plot_predictions(train_df, predictions, image_ids, img_size_ori=101, max_images = 60, grid_width = 15, figsize=()):
+    plt.suptitle(
+        "Green: salt. Top-left: coverage class, top-right: salt coverage, bottom-left: depth"
+    )
+
+
+def plot_predictions(
+    train_df,
+    predictions,
+    image_ids,
+    img_size_ori=101,
+    max_images=60,
+    grid_width=15,
+    figsize=(),
+):
     grid_height = int(max_images / grid_width)
     figsize = (grid_width, grid_height) if figsize == () else figsize
     fig, axs = plt.subplots(grid_height, grid_width, figsize=figsize)
@@ -55,9 +77,51 @@ def plot_predictions(train_df, predictions, image_ids, img_size_ori=101, max_ima
         ax.imshow(mask, alpha=0.3, cmap="Greens")
         ax.imshow(pred, alpha=0.3, cmap="OrRd")
         ax.text(1, img_size_ori - 1, train_df.loc[idx].z, color="black")
-        ax.text(img_size_ori - 1, 1, round(train_df.loc[idx].coverage, 2), color="black", ha="right", va="top")
-        ax.text(1, 1, train_df.loc[idx].coverage_class, color="black", ha="left", va="top")
+        ax.text(
+            img_size_ori - 1,
+            1,
+            round(train_df.loc[idx].coverage, 2),
+            color="black",
+            ha="right",
+            va="top",
+        )
+        ax.text(
+            1, 1, train_df.loc[idx].coverage_class, color="black", ha="left", va="top"
+        )
+        if 'iou' is in train_df:
+            ax.text(
+                1, 1, train_df.loc[idx].iou, color="black", ha="left", va="bottom"
+            )
         ax.set_yticklabels([])
         ax.set_xticklabels([])
-    plt.suptitle("Green: salt, Red: prediction. Top-left: coverage class, top-right: salt coverage, bottom-left: depth")
-    
+    plt.suptitle(
+        "Green: salt, Red: prediction. Top-left: coverage class, top-right: salt coverage, bottom-left: depth"
+    )
+
+
+def plot_poor_predictions(
+    train_df,
+    predictions,
+    labels,
+    image_ids,
+    iou_threshold=0.0,
+    img_size_ori=101,
+    max_images=60,
+    grid_width=15,
+    figsize=(),
+):
+    iou_iter = (
+        _thresholded_iou_for(1, label, pred) for pred, label in zip(predictions, labels)
+    )
+    iou_series = pd.Series(list(iou_iter), index=image_ids)
+    image_ids = iou_series[iou_series <= iou_threshold].index
+    train_df['iou'] = iou_series
+    plot_predictions(
+        train_df,
+        predictions,
+        image_ids,
+        img_size_ori=img_size_ori,
+        max_images=max_images,
+        grid_width=grid_width,
+        figsize=figsize,
+    )
