f5d0c6db33cc844b9930a0edda70730fe965b207
MSalvaris <msalvaris@users.noreply.github.com>
2018-10-05 15:20:17 +0100

    Adding git tools for looking up and setting description

---
+++ src/git_tools.py
@@ -0,0 +1,29 @@
+import fire
+from git import Repo
+
+
+def _get_repo():
+    return Repo(search_parent_directories=True)
+
+
+def _is_branch(section):
+    return section.startswith("branch")
+
+
+def list_branches():
+    r = _get_repo()
+    repo_reader = r.config_reader()
+    sections = repo_reader.sections()
+    for branch in filter(_is_branch, sections):
+        print(repo_reader.items(branch))
+
+
+def set_description(branch, description):
+    r = _get_repo()
+    repo_writer = r.config_writer()
+    repo_writer.set(f"branch '{branch}'", "description", description)
+    repo_writer.release()
+
+
+if __name__ == "__name__":
+    fire.Fire({"list": list_branches, "description": set_description})
