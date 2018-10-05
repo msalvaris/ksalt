8221c366fa4fbacd5a9a8ef63a57f64599c1edad
MSalvaris <msalvaris@users.noreply.github.com>
2018-10-05 16:09:04 +0100

    Adds ability to use current branch

--- src/git_tools.py
+++ src/git_tools.py
@@ -18,20 +18,22 @@ def list_branches():
         print(repo_reader.items(branch))
 
 
-def set_description(branch, description):
+def set_description(description, branch=None):
     r = _get_repo()
+    if branch is None:
+        branch = r.active_branch.name
     repo_writer = r.config_writer()
     repo_writer.set('branch "{}"'.format(branch), "description", description)
     repo_writer.release()
 
 
 class Description(object):
-    def set(self, branch, description):
+    def set(self, description, branch=None):
         set_description(branch, description)
 
-    def set_from_file(self, branch, filename):
+    def set_from_file(self, filename, branch=None):
         description = open(filename).readlines()
-        set_description(branch, description)
+        set_description(description, branch=branch)
 
 
 if __name__ == "__main__":
