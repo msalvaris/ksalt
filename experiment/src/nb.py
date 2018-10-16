2ff786dee59616ff0bf45d867d082d594257d0e2
MSalvaris <msalvaris@users.noreply.github.com>
2018-10-16 18:26:57 +0100

    Adds generation of config to nb script

--- experiment/src/nb.py
+++ experiment/src/nb.py
@@ -1,8 +1,12 @@
 import papermill as pm
+from config import generate
 
 
-def _execute(input_nb, output_nb):
+def _execute(input_nb, output_nb, generate_config=True):
+    if generate_config:
+        generate()
     pm.execute_notebook(input_nb, output_nb, log_output=True, progress_bar=False)
+    # Save config to model directory
 
 
 if __name__ == "__main__":
