fbc62afa6fb7427e6bcddd50afe8d7521c5c58c5
MSalvaris <msalvaris@users.noreply.github.com>
2018-10-18 15:02:11 +0100

    Moving logger default behind if clause to prevent excessive logging in notebook

--- experiment/src/config.py
+++ experiment/src/config.py
@@ -5,7 +5,9 @@ import uuid
 
 import fire
 
-logging.basicConfig(level=logging.INFO)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    
 logger = logging.getLogger(__name__)
 
 
